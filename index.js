656fb72ee26f4988b1a1f005989345ae2d4cfc56
Harum Helmy <harumendhah@gmail.com>
2019-08-07 18:36:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    let configObject = {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    };
+
+    return fetch("http://localhost:3000/users", configObject)
+        .then(response => response.json())
+        .then(object => document.body.innerHTML = object["id"])
+        .catch(err => document.body.innerHTML = err.message)
+}
